3cff31874518ca98a3c79011184166a6214cb529
EC2 Default User <ec2-user@ip-172-31-16-215.us-west-2.compute.internal>
2023-10-24 14:32:56 +0000

    changes to support cloud testing for labs

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "TAYOLI-LAB"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "Gitlab_Test_Lab"
     }
   }
 }
